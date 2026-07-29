fd90acdf98f6f74eb7a4f5b6547c9a42c6607e1a
WhyNotHmm <157852306+WhyNotHmm@users.noreply.github.com>
2026-07-28 20:00:54 -0400

    Particles

---
+++ Nature's Bidding/Assets/Scripts/UI/BiddingVFXManager.cs
@@ -0,0 +1,35 @@
+using Cysharp.Threading.Tasks;
+using System;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+public class BiddingVFXManager : MonoBehaviour
+{
+    [Header("Particle Prefabs")]
+    [SerializeField] GameObject coinTransfer;
+
+    public void SpawnCoinTransfer(int coinCount, GameObject target, GameObject source) //pass number of coins, and gameobject to target.
+    {
+        GameObject go = Instantiate(coinTransfer, weaponHolderTransform, false);
+        go.transform.localPosition = Vector3.zero;
+        go.transform.localRotation = Quaternion.identity;
+
+        SafeDispose(go, coinCoint/30).Forget();
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
